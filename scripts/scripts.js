340a36d79249a06699a646c3ffdbd49285c45184
Dudi1896 <denzelu181@gmail.com>
2022-11-07 12:32:42 -0500

    Navbar completed

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+function responsiveNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function menuBar(x) {
+    x.classList.toggle("change");
+  }
